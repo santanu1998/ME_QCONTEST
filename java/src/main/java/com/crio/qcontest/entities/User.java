93b4483eb24752f22250921743ea41d33fc6dbdd
Santanu Singha <69622683+santanu1998@users.noreply.github.com>
2023-02-22 14:15:50 +0530

    first commit

---
+++ java/src/main/java/com/crio/qcontest/entities/User.java
@@ -0,0 +1,47 @@
+package com.crio.qcontest.entities;
+
+public class User {
+    private final String name;
+    private Integer totalScore;
+    private final Long id;
+
+    public User(String name, Long id) {
+        this.name = name;
+        this.id = id;
+        this.totalScore = 100;
+    }
+
+    public User(String name) {
+        this(name, null);
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getTotalScore() {
+        return totalScore;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    // TODO: CRIO_TASK_MODULE_ENTITIES
+    // Complete the implementation of modifyScore method
+    // Implementation must take care of the following cases:-
+    // 1) Set an appropriate totalScore.
+    // 2) Throw a Runtime Exception with an appropriate message for invalid score.
+
+    public void modifyScore(Integer score){
+        if (score < 0 || score > 100) {
+            throw new RuntimeException("Invalid score: score must be between 0 and 100 (inclusive).");
+        }
+        this.totalScore = score;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + "]";
+    }  
+}
---
+++ java/src/main/java/com/crio/qcontest/services/UserService.java
@@ -0,0 +1,44 @@
+package com.crio.qcontest.services;
+
+ import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.crio.qcontest.constants.UserOrder;
+import com.crio.qcontest.entities.User;
+import com.crio.qcontest.repositories.IUserRepository;
+
+public class UserService{
+
+    private final IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // TODO: CRIO_TASK_MODULE_SERVICES
+    // Complete the implementation of createUser method
+    // Implementation must take care of the following cases:-
+    // 1) Create and store user in the repository.
+
+    public User createUser(String name) {
+        User user = new User(name);
+        return userRepository.save(user);
+    }
+
+    // TODO: CRIO_TASK_MODULE_SERVICES
+    // Complete the implementation of getAllUser method
+    // Implementation must take care of the following cases:-
+    // 1) Get all the users in ascending Order w.r.t score.
+    // 2) Get all the users in descending Order w.r.t score.
+
+    public List<User> getUsers(UserOrder userOrder) {
+        List<User> users = userRepository.findAll();
+        if (userOrder == UserOrder.SCORE_ASC) {
+            users.sort((u1, u2) -> u1.getTotalScore() - u2.getTotalScore());
+        } else if (userOrder == UserOrder.SCORE_DESC) {
+            users.sort((u1, u2) -> u2.getTotalScore() - u1.getTotalScore());
+        }
+        return users;
+    } 
+}
